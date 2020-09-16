59482347fd18a7334252b4a4fa1912fa48aa5b1a
bettilaszlo <laszlobetti3@gmail.com>
2020-09-16 09:30:08 +0200

    Egy királynő megjelenítése

--- Kiralyno/Program.cs
+++ Kiralyno/Program.cs
@@ -16,7 +16,22 @@ namespace Kiralyno
 
         public void Elhelyez()
         {
+            //1. véletlen helyiérték létrehozása:
+            //      - Random osztály érték készlete: [0,7]
+            //       - véletlen sor és oszlop kell  
+            //        - elhelyezzük a "K"-t csak akkor, 
+            //                 HA üres ---> "#"
 
+            Random vel = new Random();
+            int sor = vel.Next(0, 8);
+            int oszlop = vel.Next(0, 8);
+
+            if (T[sor,oszlop] == '#')
+            {
+                T[sor, oszlop] = 'K';
+            }
+
+            T[sor, oszlop] = 'K';
         }
         public void FajlbaIr()
         {
@@ -24,7 +39,14 @@ namespace Kiralyno
         }
         public void Megjelenit()
         {
-
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write($"{T[i, j]} ");
+                }
+                Console.WriteLine();
+            }
         }
         public Tabla(char ch)
         {
@@ -56,16 +78,11 @@ namespace Kiralyno
         {
             Console.WriteLine("Királynők feladat");
             Tabla t = new Tabla('#');
-
             Console.WriteLine("Üres tábla:");
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    Console.WriteLine($"{T[i,j]} ");
-                }
-                Console.WriteLine();
-            }
+
+            t.Megjelenit();
+            t.Elhelyez();
+            Console.WriteLine();
             t.Megjelenit();
 
             Console.ReadKey();
