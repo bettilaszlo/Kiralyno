d2b58c6040083d6e83f09411f7313267c9c90d36
bettilaszlo <laszlobetti3@gmail.com>
2020-09-16 08:35:14 +0200

    Add project files.

---
+++ Kiralyno/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralyno
+{
+    class Tabla
+    {
+        private char[,] tomb = new char[8,8]; 
+        private char UresCella;
+        private int UresOszlopokSzama;
+        private int UresSorokSzama;
+
+    }
+    class Program
+    {
+        public void Elhelyez()
+        {
+
+        }
+        public void FajlbaIr()
+        {
+
+        }
+        public void Megjelenit()
+        {
+
+
+        }
+        public Tabla(char ch)
+        {
+
+        }
+
+        public int UresOszlop()
+        {
+
+        }
+
+        public int UresSor()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+
+        }
+    }
+}
